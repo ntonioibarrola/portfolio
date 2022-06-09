8a6005bbf19dfc74e325dbb93ce32c661ae898ec
Antonio Ibarrola <59556185+ntonioibarrola@users.noreply.github.com>
2022-06-08 17:27:02 -0700

    TypeScript, Tailwind, Prettier, and ESLint setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='h-full p-8 text-slate-800'>
+      <h1 className='font-display text-center text-5xl text-black'>My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
