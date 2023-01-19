2d8151b5f4a6395466339f8b9c4aa5a0784b2269
Antonio Ibarrola <ntonio.ibarrola@gmail.com>
2023-01-19 14:33:35 -0800

    Add click animation to shortcuts

--- src/components/Shortcut.tsx
+++ src/components/Shortcut.tsx
@@ -2,11 +2,15 @@ import { FC, ReactNode } from 'react';
 
 const Shortcut: FC<{ children: ReactNode; source: string }> = ({ children, source }) => {
   return (
-    <div className='flex cursor-pointer flex-col items-center gap-1'>
-      <div className='h-12 w-12'>
+    <div className='group flex flex-col items-center gap-1'>
+      <div
+        className='h-12 w-12 transition-transform group-hover:scale-110 group-hover:cursor-pointer group-active:scale-100'
+        onClick={() => console.log('hello')}
+        role='button'
+      >
         <img src={source} />
       </div>
-      <div className='rounded-retro bg-froly-100 px-2 font-w95fa leading-tight tracking-wide'>
+      <div className='select-none rounded-retro bg-froly-100 px-2 font-w95fa leading-tight tracking-wide group-hover:cursor-pointer'>
         {children}
       </div>
     </div>
